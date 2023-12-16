4b2e82939f8d6fa2450970a09ba8867f6ea8a54f
Flavio Filho <flavio.filho@trinusco.com.br>
2023-12-16 11:40:59 -0300

    update pokemons.json

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { css } from "../../styled-system/css";
 import { PokemonCard } from "./components/PokemonCard/PokemonCard";
 import { PokemonTypeBadge } from "./components/PokemonTypeBadge/PokemonTypeBadge";
+import pokemonsData from "./api/pokemons.json";
 
 export default function Home() {
   const pokemonTypes = [
@@ -52,18 +53,14 @@ export default function Home() {
           padding: "10px",
         })}
       >
-        <PokemonCard
-          PokemonNumber="0001"
-          pokemonImgUrl="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png"
-          pokemonName="Bulbasaur"
-          pokemonImgAlt="Bulbasaur"
-        />
-        <PokemonCard
-          PokemonNumber="0004"
-          pokemonImgUrl="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png"
-          pokemonName="Charmander"
-          pokemonImgAlt="Charmander"
-        />
+        {pokemonsData.map((pokemonData) => (
+          <PokemonCard
+            PokemonNumber={pokemonData.number}
+            pokemonImgUrl={pokemonData.ThumbnailImage}
+            pokemonName={pokemonData.name}
+            pokemonImgAlt={pokemonData.name}
+          />
+        ))}
       </div>
     </div>
   );
