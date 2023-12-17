5453e7a8ec3902b9313692d522a28c92b77054b5
Flavio Filho <flavio.filho@trinusco.com.br>
2023-12-17 11:14:46 -0300

    finalized filtering logic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,20 @@ import { PokemonCard } from "./components/PokemonCard/PokemonCard";
 import pokemonsData from "./api/pokemons.json";
 import { css } from "../../styled-system/css/css";
 import { TextField } from "./components/TextField/TextField";
-import { useState } from "react";
-
+import { useRouter } from "../../node_modules/next/router";
 export default function Home() {
-  const [search, setSearch] = useState("");
+  const router = useRouter();
+
+  const filterValue = router.query.filtro
+    ? String(router.query.filtro).toLowerCase()
+    : undefined;
+  const filteredPokemons = filterValue
+    ? pokemonsData.filter((pokemonData) => {
+        const pokemonName = pokemonData.name.toLowerCase();
+
+        return pokemonName.includes(filterValue);
+      })
+    : pokemonsData;
   return (
     <div
       className={css({
@@ -24,7 +34,7 @@ export default function Home() {
           padding: "10px",
         })}
       >
-        {pokemonsData.map((pokemonData) => (
+        {filteredPokemons.map((pokemonData) => (
           <PokemonCard
             PokemonNumber={pokemonData.number}
             pokemonImgUrl={pokemonData.urlImage}
