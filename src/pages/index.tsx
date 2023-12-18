5247649274a1d085436b108ab83d127d259040af
Flavio Filho <flavio.filho@trinusco.com.br>
2023-12-17 22:25:53 -0300

    removed unused imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,3 @@
-import { PokemonCard } from "./components/PokemonCard/PokemonCard";
-import { css } from "../../styled-system/css/css";
-import { useHomePage } from "./useHomePage";
 import { PokedexList } from "./components/PokedexList/PokedexList";
 
 export default function Home() {
