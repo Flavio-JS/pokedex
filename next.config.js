6227566195db31e9aa2451dd9ceb8b22715b6b55
flavio.filho <flavio.filho@trinusco.com.br>
2023-12-21 03:23:06 +0000

    removed project images

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["raw.githubusercontent.com"],
+    remotePatterns: [
+      {
+        hostname: "raw.githubusercontent.com",
+      },
+    ],
   },
 };
 
